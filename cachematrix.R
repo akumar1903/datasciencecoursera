79fced649b9db389697e8441fb08a7d8cb475f54
Arun190377 <arunkumarcg@gmail.com>
2014-12-21 12:50:28 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,46 +1,53 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
 ## Write a short comment describing this function
+## makeCacheMatrix creates a special "vector", which is really a list containing a function to
+## set the value of the vector
+## get the value of the vector
+## set the value of the mean
+## get the value of the mean
 
-makeCacheMatrix <- function(x = matrix()) {
+CacheMatrix <- function(x = matrix()) {
   
-  xinv <- NULL # this is where the result of inversion is stored
-  # A setter function, use this to set a matrix to object created by makeCacheMatrix function
-  # e.g makeCacheMatrix(testmatrix) # here we work on testmatrix
-  # makeCacheMatrix$set(testmatrix1) # here we work on testmatrix1
+  xinverse <- NULL # return result
   set <- function(y) {
     x <<- y
-    xinv <<- NULL # it also initialises xinv to null
+    xinverse <<- NULL # initiatlize the value to null
   }
   
   get <- function() x # return the input matrix
-  setInv <- function(inv) xinv <<- inv # set the inversed matrix
-  getInv <- function() xinv # return the inversed matrix
+  setI <- function(inverse) xinverse <<- inverse # set the inversed matrix
+  getI <- function() xinverse # return the inversed matrix
   # return a list that contains these functions, so that we can use
-  # makeCacheMatrix object like these
-  # x <- makeCacheMatrix(testmatrix)
-  # x$set(newmatrix) # to change matrix
-  # x$get # to get the setted matrix
-  # x$setInv # to set the inversed matrix
-  # x$getInv # to get the inversed matrix
+  ## set the value of the vector
+  ## get the value of the vector
+  ## set the value of the mean
+  ## get the value of the mean
   list(set = set, get = get,
-       setInv = setInv,
-       getInv = getInv)
+       setI = setI,
+       getI = getI)
 }
-
-
 cacheSolve <- function(x, ...) {
-  m <- x$getInv() # get the inversed matrix from object x
-  # it will be null if uncalculated, remember the first line "xinv <- NULL" in the previous function
-  if(!is.null(m)) { # if the inversion result is there
+  m <- x$getI() # get the inversed matrix object x
+  if(!is.null(m)) { # check for inverseion result
     message("getting cached data")
-    return(m) # return the calculated inversion
+    return(m) # return the calculated value
   }
-  data <- x$get() # if not, we do x$get to get the matrix object
-  m <- solve(data) # we solve it
-  x$setInv(m) # we then set it to the object
-  m # return the solved result
+  data <- x$get() # else get the matrix object
+  m <- solve(data) # solve the data
+  x$setI(m) # set to the object
+  m # return answer
 }
+
+#test
+#> m$setI(matrix(seq(1:10),2))
+#> m$get()
+#     [,1]
+#[1,]   NA
+#> m$getI()
+#     [,1] [,2] [,3] [,4] [,5]
+#[1,]    1    3    5    7    9
+#[2,]    2    4    6    8   10
+
 
 
